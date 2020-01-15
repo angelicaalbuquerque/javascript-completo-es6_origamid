ce7aaac9ad351de238504b61a7d577ff1c584274
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-01-15 21:00:55 -0200

    Funções - parte 2

--- javascript-para-iniciantes/funcoes/funcoes.js
+++ javascript-para-iniciantes/funcoes/funcoes.js
@@ -1,6 +1,8 @@
 /***  Aula - Funções ***/
 
-//Bloco de código que pode ser executado e reutilizado. Valores podem ser passados por uma função e a mesma retorna outro valor.
+/*
+Bloco de código que pode ser executado e reutilizado. Valores podem ser passados por uma função e a mesma retorna outro valor.
+*/
 function areaQuadrado(lado) {
   return lado * lado;
 }
@@ -10,17 +12,21 @@ areaQuadrado(5) // 25
 areaQuadrado(2) // 4
 //Chamada de function declaration
 
+
 //FUNÇÕES
 function pi() {
   return 3.14;
 }
 
 var total = 5 * pi(); // 15.7
-//CParênteses () executam uma função
+//Parênteses () executam uma função
 
-//PARÂMETROS E ARGUMENTOS
-//Ao criar uma função, você pode definir parâmetros.
-//Ao executar uma função, você pode passar argumentos.
+
+/* 
+PARÂMETROS E ARGUMENTOS
+Ao criar uma função, você pode definir parâmetros.
+Ao executar uma função, você pode passar argumentos. 
+*/
 
 // peso e altura são os parâmetros
 function imc(peso, altura) {
@@ -33,7 +39,8 @@ function imc(peso, altura) {
 
 //Separar por vírgula cada parâmetro. Você pode definir mais de um parâmetro ou nenhum também
 
-//PARÊNTESES EXECUTA A FUNÇÃO
+
+/* PARÊNTESES EXECUTA A FUNÇÃO */
 function corFavorita(cor) {
   if(cor === 'azul') {
     return 'Você gosta do céu';
@@ -45,16 +52,91 @@ function corFavorita(cor) {
 }
 corFavorita(); // retorna 'Você não gosta de nada'
 
-//Se apenas definirmos a função com o function e não executarmos a mesma, nada que estiver dentro dela irá acontecer
+/*
+Se apenas definirmos a função com o function e não executarmos a mesma, nada que estiver dentro dela irá acontecer
+*/
 
-//ARGUMENTOS PODEM SER FUNÇÕES
-//Chamadas de Callback, geralmente são funções que ocorrem após algum evento.
+
+/*
+ARGUMENTOS PODEM SER FUNÇÕES
+Chamadas de Callback, geralmente são funções que ocorrem após algum evento.
+*/
 
 addEventListener('click', function() {
   console.log('Clicou');
 });
-// A função possui dois argumentos
-// Primeiro é a string 'click'
-// Segundo é uma função anônima
+/*
+A função possui dois argumentos
+Primeiro é a string 'click'
+Segundo é uma função anônima
+*/
 
-///unções anônimas são aquelas em que o nome da função não é definido, escritas como function() {} ou () => {}
+/* 
+Funções anônimas são aquelas em que o nome da função não é definido, escritas como function() {} ou () => {} 
+*/
+
+
+/* 
+PODE OU NÃO RETORNAR UM VALOR
+Quando não definimos o return, ela irá retornar undefined. O código interno da função é executado normalmente, independente de existir valor de return ou não. 
+*/
+
+function imc(peso, altura) {
+  const imc = peso / (altura ** 2);
+  console.log(imc);
+}
+
+imc(80, 1.80); // retorna o imc
+console.log(imc(80, 1.80)); // retorna o imc e undefined
+
+
+/*
+VALORES RETORNADOS
+Uma função pode retornar qualquer tipo de dado e até outras funções.
+*/
+
+function terceiraIdade(idade) {
+  if(typeof idade !== 'number') {
+    return 'Informe a sua idade!';
+  } else if(idade >= 60) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Cuidado, retornar diferentes tipos de dados na mesma função não é uma boa ideia.
+
+
+/*
+ESCOPO
+Variáveis e funções definidas dentro de um bloco {}, não são visíveis fora dele.
+*/
+
+function precisoVisitar(paisesVisitados) {
+  var totalPaises = 193;
+  return `Ainda faltam ${totalPaises - paisesVisitados} paises para visitar`
+}
+console.log(totalPaises); // erro, totalPaises não definido
+
+
+/*
+ESCOPO LÉXICO
+Funções conseguem acessar variáveis que foram criadas no contexto pai
+*/
+
+var profissao = 'Designer';
+
+function dados() {
+  var nome = 'André';
+  var idade = 28;
+  function outrosDados() {
+    var endereco = 'Rio de Janeiro';
+    var idade = 29;
+    return `${nome}, ${idade}, ${endereco}, ${profissao}`;
+  }
+  return dados();
+}
+
+dados(); // Retorna 'André, 29, Rio de Janeiro, Designer'
+outrosDados(); // retorna um erro
