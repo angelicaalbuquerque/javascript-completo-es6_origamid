8c1d1c8db2d3ff5cc0488b81644791aad1c85a8e
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-01-20 00:44:55 -0200

    Exercícios de Objetos

--- javascript-para-iniciantes/objetos/objetos_exercicio.js
+++ javascript-para-iniciantes/objetos/objetos_exercicio.js
@@ -3,7 +3,18 @@
 // Crie um objeto com os seus dados pessoais
 // Deve possui pelo menos duas propriedades nome e sobrenome
 
+var dados = {
+  nome: "Angélica",
+  sobrenome: "Albuquerque",
+  idade: 29,
+  areaProfissao: "TI",
+}
+
 // Crie um método no objeto anterior, que mostre o seu nome completo
+
+dados.nomeCompleto = function() {
+  return `${this.nome} ${this.sobrenome}`
+}
 
 // Modifique o valor da propriedade preco para 3000
 var carro = {
@@ -12,6 +23,20 @@ var carro = {
   marca: 'Audi',
 }
 
-// Crie um objeto de um cachorro que represente um labrador,
-// preto com 10 anos, que late ao ver um homem
+carro.preco = 3000;
+
+// Crie um objeto de um cachorro que represente um labrador, preto com 10 anos, que late ao ver um homem
+
+var cachorro = {
+  raca: "labrador",
+  cor: "preto",
+  idade: "10",
+  latir(pessoa){
+    if (pessoa === "homem") {
+      return "Latir"
+    } else {
+      return "Não late"
+    }
+  }
+}
 
