8b3826b613a7b34b0e51e5cc52fdde3425cc1f20
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-01-27 22:38:50 -0200

    Exercícios - O Que é DOM

--- 03-dom-para-iniciantes/o-que-e-dom/o-que-e-dom_exercicio.js
+++ 03-dom-para-iniciantes/o-que-e-dom/o-que-e-dom_exercicio.js
@@ -2,9 +2,22 @@
 
 // Retorne o url da página atual utilizando o objeto window
 
-// Seleciona o primeiro elemento da página que
-// possua a classe ativo
+paginaAtual = window.location.href;
+console.log(paginaAtual); 
+
+if (paginaAtual === "http://127.0.0.1:5500/03-dom-para-iniciantes/o-que-e-dom/") {
+  console.log("Você está na página atual");
+}
+
+// Seleciona o primeiro elemento da página que possua a classe ativo
+
+const elementoAtivo = document.querySelector(".ativo");
 
 // Retorne a linguagem do navegador
 
+const linguagemNav = window.navigator.language;
+console.log(linguagemNav);
+
 // Retorne a largura da janela 
+const larguraJanela = window.innerWidth;
+console.log(larguraJanela);
