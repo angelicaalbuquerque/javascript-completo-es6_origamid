8a079124993ca5cf855256b1b2b62e62a8a334fb
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-02-22 01:01:58 -0300

    Exercícios - Seleção de Elementos

--- 03-dom-para-iniciantes/selecao-de-elementos/script.js
+++ 03-dom-para-iniciantes/selecao-de-elementos/script.js
@@ -99,13 +99,29 @@ gridSection.forEach(function(gridItem, index, array) {
 */
 
 
-// E X E R C Í C I O
+// E X E R C Í C I O S
 // Retorne no console todas as imagens do site
+const img = document.querySelectorAll("img");
+console.log(img);
+
+// ou
+const img2 = document.getElementsByTagName("img");
+console.log(img2);
 
 // Retorne no console apenas as imagens que começaram com a palavra imagem
+const imagemAnimais = document.querySelectorAll('img[src^="./img/imagem"]');
+console.log(imagemAnimais);
 
 // Selecione todos os links internos (onde o href começa com #)
+const linksInternos = document.querySelectorAll('[href^="#"]');
+console.log(linksInternos);
 
 // Selecione o primeiro h2 dentro de .animais-descricao
+const primeiroH2 = document.querySelector(".animais-descricao h2");
+console.log(primeiroH2);
 
 // Selecione o último p do site
+const paragrafos = document.querySelectorAll("p");
+console.log(paragrafos[--paragrafos.length]);
+
+
