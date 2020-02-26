bfeb1c69112fc03c2deb45c84ab7286ce8517349
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-02-25 22:11:08 -0300

    Exercícios - Classes e Atributos

--- 03-dom-para-iniciantes/classes-e-atributos/script.js
+++ 03-dom-para-iniciantes/classes-e-atributos/script.js
@@ -73,10 +73,28 @@ Lembre-se que podemos modificar o valor de uma propriedade objeto.propriedade = 
 // E X E R C Í C I O S
 
 // Adicione a classe ativo a todos os itens do menu
+const itensMenu = document.querySelectorAll(".menu a");
+itensMenu.forEach((item) => {
+  item.classList.add("ativo");
+});
 
 // Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
 
+itensMenu.forEach((item) => {
+  item.classList.remove("ativo");
+});
+
+itensMenu[0].classList.add("ativo");
+
 // Verifique se as imagens possuem o atributo alt
+const imgs = document.querySelectorAll("img");
+
+imgs.forEach((imagem) => {
+  const possuiAtributo = imagem.hasAttribute("alt");
+  console.log(imagem, possuiAtributo);
+});
 
 // Modifique o href do link externo no menu
+const link = document.querySelector('a[href^="http"]');
 
+link.setAttribute("href", "https://www.google.com/");
