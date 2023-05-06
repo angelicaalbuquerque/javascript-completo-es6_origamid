6549c8ecbf901bcef2106595735d10928de9e4b4
Angelica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-05-06 18:10:38 -0300

    [Javascript] Tipos de Dados

---
+++ 01-javascript/0102-tipos-de-dados/0102-tipos-de-dados-exercicio.js
@@ -0,0 +1,23 @@
+// Declare uma variável contendo uma string
+var nacionalidade = "brasileira";
+
+// Declare uma variável contendo um número dentro de uma string
+var anual = "12";
+
+// Declare uma variável com a sua idade
+var idade = 32;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = "Angélica";
+var sobrenome = "Albuquerque";
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+var fraseTempo = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
+
+
+console.log(nacionalidade, anual, idade, nomeCompleto, fraseTempo);
---
+++ 01-javascript/0102-tipos-de-dados/0102-tipos-de-dados.js
@@ -0,0 +1,27 @@
+var nome = 'André'; // String
+var idade = 28; // Number
+var possuiFaculdade = true; // Boolean
+var time; // Undefined
+var comida = null; // Null
+var simbolo = Symbol() // Symbol
+var novoObjeto = {} // Object
+
+console.log(typeof comida);
+
+
+var primeiroNome = "Angélica";
+var sobrenome = "Albuquerque";
+var nomeCompleto = primeiroNome + " " + sobrenome;
+console.log(nomeCompleto);
+
+
+var gols = 1000;
+var frase = "Romário fez " + gols + " gols";
+console.log(frase);
+console.log(typeof frase);
+
+var frase2 = `Romário fez ${gols} gols`; // Utilizando Template String
+console.log(frase2);
+console.log(typeof frase2);
+
+
