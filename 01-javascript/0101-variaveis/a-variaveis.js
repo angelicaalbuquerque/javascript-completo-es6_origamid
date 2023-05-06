0a7c2f6c77799cd6826095a3da815708781096eb
Angelica Albuquerque <angelica.o.albuquerque@gmail.com>
2023-05-06 00:29:27 -0300

    [Javascript] Variáveis

---
+++ 01-javascript/0101-variaveis/a-variaveis.js
@@ -0,0 +1,45 @@
+var nome = "Angélica";
+var idade = 32;
+var possuiFaculdade = true;
+console.log(nome, idade, possuiFaculdade);
+
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = preco * totalComprado;
+console.log(totalPreco);
+
+
+var sobrenome = "Albuquerque", 
+    cidade = "Rio de Janeiro", 
+    bairro = "Vila Valqueire";
+console.log(sobrenome, cidade, bairro);
+
+
+var semValorDefinido;
+console.log(semValorDefinido);
+
+
+// console.log(aindaNaoDefinida);
+
+
+//Hoisting
+var comida;
+console.log(comida);
+var comida = "Pizza";
+
+
+var time = "Vasco";
+time = "São Paulo";
+console.log(time);
+
+
+let timeBaseball = "Red Sox";
+timeBaseball = "Yankees";
+console.log(timeBaseball);
+
+
+// const timeBasquete = "Celtics";
+// timeBasquete = "Lakers";
+// console.log(timeBasquete);
+
---
+++ 01-javascript/0101-variaveis/b-variaveis-exercicio.js
@@ -0,0 +1,17 @@
+// Declarar uma variável com o seu nome
+var nome = "Angélica";
+
+// Declarar uma variável com a sua idade
+var idade = 32;
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Pizza";
+
+// Declarar 5 variáveis diferentes sem valores
+var cidade, bairro, estado, rua, pais;
+
+
+console.log(nome, idade, comidaFavorita, cidade, bairro, estado, rua, pais);
